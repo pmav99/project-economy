8517959064fbfa6f6a3c42ed2cf8692a21e25ff7
Panos Mavrogiorgos <pmav99@gmail.com>
2015-02-10 10:50:23 +0200

    Update conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,30 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Εγώ'
+AUTHOR = 'Θανάσης Αργυρίου & Παναγιώτης Μαυρογιώργος'
 SITENAME = 'Economy Project'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
-
+TIMEZONE = 'Europe/Athens'
+LOCALE = "el_GR"
 DEFAULT_LANG = 'el'
+
+#MENUITEMS = [
+    #("google", "http://www.google.com"),
+    #("yahoo", "http://www.google.com"),
+    #("bing", "http://www.google.com"),
+#]
+
+SHOW_ARTICLE_AUTHOR = True
+#SHOW_ARTICLE_CATEGORY = True
+#SHOW_DATE_MODIFIED = True
+
+#BOOTSTRAP_FLUID = True
+DISPLAY_BREADCRUMBS = True
+DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+#DISPLAY_ARTICLE_INFO_ON_INDEX = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,7 +44,17 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 4
+
+THEME = "../themes/pelican-bootstrap3"
+THEME = "./pelican-bootstrap3"
+BOOTSTRAP_THEME = "superhero"
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+
+DELETE_OUTPUT_DIRECTORY = True
+
 PLUGIN_PATHS = ['./plugins', '../plugins']
 PLUGINS = [
     "assets",
